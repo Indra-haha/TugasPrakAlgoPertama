ad6819c687fb6f174c5516094c1901b9fa7a403b
Indra-haha <124240180@student.upnyk.ac.id>
2025-03-05 21:53:42 +0700

    quick lumayan, sequential juga

--- tugasAlgoPrak.cpp
+++ tugasAlgoPrak.cpp
@@ -3,10 +3,9 @@
 #include <string>
 using namespace std;
 
-int a = 0, temp = 0;
+int a = 0, temp = 0; // temp = total data sementara
 int n;
 char pil, home;
-string cari;
 struct buku
 {
     string judul;
@@ -65,6 +64,13 @@ int tampilanHeader()
     cout << setw(99) << setiosflags(ios::left) << "-";
     cout << endl;
     cout << setfill(' ');
+    return 0;
+}
+
+int tampilanHeaderSearch()
+{
+    cout << "Judul Buku" << setw(10) << setiosflags(ios::right) << "|" << setw(2) << setiosflags(ios::right) << " ";
+    cout << "Tahun terbit" << setw(3) << setiosflags(ios::right) << "|";
     return 0;
 }
 
@@ -200,15 +206,123 @@ int bubble(int k, int temp, buku *manipulasi)
     return 0;
 }
 
-int quick(int k, int temp, buku *manipulasi)
+int quick(int k, buku *manipulasi, int awal, int akhir)
 {
-    cout << "belum ada";
+    int low = awal, high = akhir;
+    int pivot = manipulasi[(awal + akhir) / 2].terbit;
+    if (k >= 1 && k <= 5)
+    {
+        do
+        {
+            while (manipulasi[low].terbit < pivot)
+                low++;
+            while (manipulasi[high].terbit > pivot)
+                high--;
+
+            if (low <= high)
+            {
+                swap(manipulasi[low], manipulasi[high]);
+                low++;
+                high--;
+            }
+        } while (low <= high);
+        if (awal < high)
+        {
+            quick(k, manipulasi, awal, high);
+        }
+        if (low < akhir)
+        {
+            quick(k, manipulasi, low, akhir);
+        }
+    }
+    else if (k >= 6 && k <= 10)
+    {
+        cout << "Belum ada" << endl;
+    }
+    tampilanHeader();
+    for (int b = 0; b < temp; b++)
+    {
+        for (int k = 0; k < 5; k++)
+        {
+            cout << setw(2) << " ";
+            switch (k)
+            {
+            case 0:
+                cout << manipulasi[b].judul << setw(20 - manipulasi[b].judul.size()) << setiosflags(ios::right) << "|";
+                break;
+            case 1:
+                cout << manipulasi[b].pengarang << setw(20 - manipulasi[b].pengarang.size()) << setiosflags(ios::right) << "|";
+                break;
+            case 2:
+                cout << manipulasi[b].penerbit << setw(20 - manipulasi[b].penerbit.size()) << setiosflags(ios::right) << "|";
+                break;
+            case 3:
+                cout << manipulasi[b].terbit << setw(15 - (to_string(manipulasi[b].terbit)).size()) << setiosflags(ios::right) << "|";
+                break;
+            case 4:
+                cout << manipulasi[b].harga << setw(15 - (to_string(manipulasi[b].harga)).size()) << setiosflags(ios::right) << "|";
+                break;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Kembali ke menu utama [y/n] : ";
+    cin >> home;
+    if (home == 'y')
+    {
+        delete[] manipulasi;
+    }
     return 0;
 }
 
-int sequential_search()
+int sequential_search(int temp, buku *data)
 {
-    cout << "belum ada";
+    if (temp >= 3)
+    {
+        int i, x;
+        bool found;
+        cout << "Masukkan tahun terbit buku yang anda cari = ";
+        cin >> x;
+        i = 0;
+        found = false;
+        while ((i <= temp) && (!found))
+        {
+            if (data[i].terbit == x)
+            {
+                found = true;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (i <= temp && (found))
+        {
+            cout << endl;
+            tampilanHeaderSearch();
+            cout << endl;
+            for (int k = 0; k < 2; k++)
+            {
+                switch (k)
+                {
+                case 0:
+                    cout << data[i].judul << setw(20 - data[i].judul.size()) << setiosflags(ios::right) << "|" << setw(2) << setiosflags(ios::right) << "|";
+                    break;
+                case 1:
+                    cout << data[i].terbit << setw(15 - (to_string(data[i].terbit)).size()) << setiosflags(ios::right) << "|";
+                    break;
+                }
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "Buku yg terbit pada tahun " << x << " tidak tersedia." << endl;
+        }
+    }
+    cout << endl;
+    cout << "Kembali ke menu utama [y/n] : ";
+    cin >> home;
     return 0;
 }
 
@@ -235,7 +349,6 @@ int main()
     {
         system("cls");
         home = 'n';
-        buku *manipulasi;
         cout << "----MENU----" << endl;
         cout << "1. Input Data Buku" << endl;
         cout << "2. Tampilkan Data Buku" << endl;
@@ -245,6 +358,7 @@ int main()
         cout << "6. Cari Buku dengan Binary Search" << endl;
         cout << "7. Keluar" << endl;
         cout << "Masukan pilihan : ";
+        buku *manipulasi = new buku[temp];
         cin >> pil;
         switch (pil)
         {
@@ -264,28 +378,27 @@ int main()
             break;
         case '3':
             system("cls");
-            int urutan;
-            cout << "-----DATA AWAL SEBELUM DI URUTKAN-----" << endl;
-            dataBuku();
-            cout << "Pengurutan Data dengan Bubble Sort" << endl;
-            cout << "1. Urutkan data secara ascending : " << endl;
-            cout << "2. Urutkan data secara descending : " << endl;
-            cout << "Masukan pilihan : ";
-            cin >> urutan;
-            switch (urutan)
+            if (temp >= 10)
             {
-                char pilPengurutan;
-            case 1:
-                cout << endl;
-                daftarPilUrutan();
-                cout << "Masukan pilihan : ";
-                cin >> pilPengurutan;
-                system("cls");
-                cout << "-----DATA SEBELUM DI URUTKAN-----" << endl;
+                int urutan;
+                cout << "-----DATA AWAL SEBELUM DI URUTKAN-----" << endl;
                 dataBuku();
-                if (temp > 0)
+                cout << "Pengurutan Data dengan Bubble Sort" << endl;
+                cout << "1. Urutkan data secara ascending : " << endl;
+                cout << "2. Urutkan data secara descending : " << endl;
+                cout << "Masukan pilihan : ";
+                cin >> urutan;
+                switch (urutan)
                 {
-                    buku *manipulasi = new buku[temp];
+                    char pilPengurutan;
+                case 1:
+                    cout << endl;
+                    daftarPilUrutan();
+                    cout << "Masukan pilihan : ";
+                    cin >> pilPengurutan;
+                    system("cls");
+                    cout << "-----DATA SEBELUM DI URUTKAN-----" << endl;
+                    dataBuku();
                     salinData(data, manipulasi, temp);
                     cout << endl;
                     cout << "-----DATA SETELAH DI URUTKAN-----" << endl;
@@ -312,21 +425,15 @@ int main()
                         bubble(5, temp, manipulasi);
                         break;
                     }
-                    cout << "Kembali ke menu utama [y/n] : ";
-                    cin >> home;
                     break;
-                }
-            case 2:
-                cout << endl;
-                daftarPilUrutan();
-                cout << "Masukan pilihan : ";
-                cin >> pilPengurutan;
-                system("cls");
-                cout << "-----DATA SEBELUM DI URUTKAN-----" << endl;
-                dataBuku();
-                if (temp > 0)
-                {
-                    buku *manipulasi = new buku[temp];
+                case 2:
+                    cout << endl;
+                    daftarPilUrutan();
+                    cout << "Masukan pilihan : ";
+                    cin >> pilPengurutan;
+                    system("cls");
+                    cout << "-----DATA SEBELUM DI URUTKAN-----" << endl;
+                    dataBuku();
                     salinData(data, manipulasi, temp);
                     cout << endl;
                     cout << "-----DATA SETELAH DI URUTKAN-----" << endl;
@@ -353,35 +460,129 @@ int main()
                         bubble(10, temp, manipulasi);
                         break;
                     }
-                    cout << "Kembali ke menu utama [y/n] : ";
-                    cin >> home;
                     break;
                 }
             }
-            break;
-        case '4':
-            cout << "Pengurutan Data dengan Quick Sort" << endl;
-            cout << "1. Urutkan data secara ascending : " << endl;
-            cout << "2. Urutkan data secara descending : " << endl;
-            cout << "Masukan pilihan : ";
-            cin >> urutan;
-            switch (urutan)
+            else
             {
-            case '1':
-                break;
-            case '2':
-                break;
+                cout << "Data buku anda kurang dari 10" << endl;
+                dataBuku();
+                cout << endl;
+                cout << "Input data buku minimal " << 10 - temp << " Data buku." << endl;
+                cout << "Kembali ke menu utama [y/n] : ";
+                cin >> home;
             }
-            break;
+        case '4':
+            system("cls");
+            if (temp >= 10)
+            {
+                int urutan;
+                cout << "-----DATA AWAL SEBELUM DI URUTKAN-----" << endl;
+                dataBuku();
+                cout << "Pengurutan Data dengan Quick Sort" << endl;
+                cout << "1. Urutkan data secara ascending : " << endl;
+                cout << "2. Urutkan data secara descending : " << endl;
+                cout << "Masukan pilihan : ";
+                cin >> urutan;
+                switch (urutan)
+                {
+                    char pilPengurutan;
+                case 1:
+                {
+                    cout << endl;
+                    daftarPilUrutan();
+                    cout << "Masukan pilihan : ";
+                    cin >> pilPengurutan;
+                    system("cls");
+                    cout << "-----DATA SEBELUM DI URUTKAN-----" << endl;
+                    dataBuku();
+                    buku *manipulasi = new buku[temp];
+                    salinData(data, manipulasi, temp);
+                    cout << endl;
+                    cout << "-----DATA SETELAH DI URUTKAN-----" << endl;
+                    switch (pilPengurutan)
+                    {
+                    case '1':
+                        cout << "Pengurutan berdasarkan Judul Buku" << endl;
+                        quick(1, manipulasi, 0, n - 1);
+                        break;
+                    case '2':
+                        cout << "Pengurutan berdasarkan Pengarang" << endl;
+                        quick(2, manipulasi, 0, n - 1);
+                        break;
+                    case '3':
+                        cout << "Pengurutan berdasarkan Penerbit" << endl;
+                        quick(3, manipulasi, 0, n - 1);
+                        break;
+                    case '4':
+                        cout << "Pengurutan berdasarkan Tahun Terbit" << endl;
+                        quick(4, manipulasi, 0, n - 1);
+                        break;
+                    case '5':
+                        cout << "Pengurutan berdasarkan Harga" << endl;
+                        quick(5, manipulasi, 0, n - 1);
+                        break;
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    cout << endl;
+                    daftarPilUrutan();
+                    cout << "Masukan pilihan : ";
+                    cin >> pilPengurutan;
+                    system("cls");
+                    cout << "-----DATA SEBELUM DI URUTKAN-----" << endl;
+                    dataBuku();
+                    buku *manipulasi = new buku[temp];
+                    salinData(data, manipulasi, temp);
+                    cout << endl;
+                    cout << "-----DATA SETELAH DI URUTKAN-----" << endl;
+                    switch (pilPengurutan)
+                    {
+                    case '1':
+                        cout << "Pengurutan berdasarkan Judul Buku" << endl;
+                        quick(6, manipulasi, 0, n - 1);
+                        break;
+                    case '2':
+                        cout << "Pengurutan berdasarkan Pengarang" << endl;
+                        quick(7, manipulasi, 0, n - 1);
+                        break;
+                    case '3':
+                        cout << "Pengurutan berdasarkan Penerbit" << endl;
+                        quick(8, manipulasi, 0, n - 1);
+                        break;
+                    case '4':
+                        cout << "Pengurutan berdasarkan Tahun Terbit" << endl;
+                        quick(9, manipulasi, 0, n - 1);
+                        break;
+                    case '5':
+                        cout << "Pengurutan berdasarkan Harga" << endl;
+                        quick(10, manipulasi, 0, n - 1);
+                        break;
+                    }
+                    break;
+                }
+                }
+            }
+            else
+            {
+                cout << "Data buku anda kurang dari 10" << endl;
+                dataBuku();
+                cout << endl;
+                cout << "Input data buku minimal " << 10 - temp << " Data buku." << endl;
+                cout << "Kembali ke menu utama [y/n] : ";
+                cin >> home;
+            }
         case '5':
+            system("cls");
             cout << "Pencarian Data dengan Sequential Search" << endl;
-            cout << "Masukan tahun terbit buku yang dicari : ";
-            cin >> cari;
+            sequential_search(temp, data);
             break;
         case '6':
+            system("cls");
             cout << "Pencarian Data dengan Binary Search" << endl;
-            cout << "Masukan tahun terbit buku yang dicari : ";
-            cin >> cari;
+            sequential_search(temp, data);
             break;
         case '7':
             cout << "Terima Kasih" << endl;
